02e65d766bcb0370e9b75328b32bd0f4c0dd552e
tallan groberg <tallan.taven@gmail.com>
2019-05-10 15:46:40 -0600

    assign numbers to face cards

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -1,1 +1,27 @@
 require './cards.rb'
+
+#make aces = 11 or 1
+
+
+# give ability to hit.
+
+j, q, k, a = 10, 10, 10, 11
+
+class BlackJack
+  def initialize
+    start_game
+  end
+  def start_game
+    puts "would you like to play(y/n)"
+    input = gets.to_s
+    case input
+    when "y"
+      puts "lets start"
+    when "n"
+      puts "no"
+    else
+      puts "invalid"
+    end
+  end
+end
+BlackJack.new
