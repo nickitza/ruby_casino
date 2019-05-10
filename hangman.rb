3a84005fcb07cc600b21a3089dc050bde3e97b99
Nicki Nixon <nickitza@gmail.com>
2019-05-10 15:14:37 -0600

    add words array

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,49 @@
 class Hangman
+  def initialize
+  
+  end
+
+  def main_menu
+
+  end
+
+  def pick_word
+    @words = [
+      "hippopotamus"
+      "puzzled"
+      "fungus"
+      "flourish"
+      "royal"
+      "branch"
+      "embezzle"
+      "supernatural"
+      "cottage"
+      "ambitious"
+      "pervasive"
+      "ridiculous"
+      "superimpose"
+      "appliance"
+      "flawless"
+      "redundant"
+      "raucous"
+      "apparatus"
+      "castle"
+      "sweltering"
+      "heartbreaking"
+      "dynamic"
+      "saxophone"
+      "juggle"
+      "regret"
+      "rustic"
+      "petite"
+      "exotic"
+      "bushel"
+      "mellow"
+      "fairies"
+      "computer"
+      "obtain"
+      "vampire"
+    ]
+  end
 
 end
