62401533e2bb38ba334eced0e550abc7b323b7b6
Nicki Nixon <nickitza@gmail.com>
2019-05-11 15:21:36 -0600

    add print_right_guesses

--- classes/hangman.rb
+++ classes/hangman.rb
@@ -6,6 +6,7 @@ class Hangman
   attr_accessor :letters_left
   def initialize
     @letters_left = ('a'..'z').to_a.join(" ")
+    @user_guess_arr = []
     main_menu
 
   end
@@ -48,33 +49,38 @@ class Hangman
   def generate_word
     clear
     puts "Your word is being generated."
-    sleep(2)
     puts "..."
+    # sleep(2)
     pick_word
-    puts "__ " * @game_word.length
-    puts @game_word
     @word_arr = @game_word.chars.to_a
     puts "Your word has been chosen. "
+    puts @game_word
     puts "Guess one letter you think is in your word."
     @guesses_left = @game_word.length
     puts "You have #{@guesses_left} guesses left."
     puts "Choose your letter wisely!"
+    puts
+    puts "Press enter to continue.".colorize(:red)
+    gets
+    clear
     get_guess
   end
 
   def get_guess
+    print_right_guesses
+    puts
+    # puts "__ " * @game_word.length
     puts "Which letter would you like to choose?"
     puts @letters_left
     @user_guess = gets.strip.downcase
     if @user_guess.length == 1
       if @word_arr.include?(@user_guess)
-          right_guess
-          get_guess
+        right_guess
+        get_guess
         else
           wrong_guess
           get_guess
       end
-      
       else
         puts "Please choose one letter to guess."
         get_guess
@@ -84,8 +90,16 @@ class Hangman
   def right_guess
     remove_letter
     puts "Hooray!! You're right! That letter is in the word!"
+    @word_arr.each_with_index do |w, i|
+      if @word_arr[i].include?(@user_guess)
+        @user_guess_arr << @user_guess
+        print @user_guess + " "
+      else
+        print "_ "
+      end
+    end
+    puts
   end
-
 
   def wrong_guess
     remove_letter
@@ -97,6 +111,18 @@ class Hangman
   def remove_letter
     @letters_left = @letters_left.delete(@user_guess)
   end
+
+  def print_right_guesses
+    @word_arr.each_with_index do |w, i|
+      if w.any? {|c| @user_guess_arr.include?(c) }
+        print @word_arr[i]
+      else
+        print "_ "
+      end
+    end
+  end
+
+  # foods.any? {|food| cheeses.include?(food) }
 
 
   def exit_to_casino
@@ -169,4 +195,4 @@ class Hangman
 
 end
 
-Hangman.new
+# Hangman.new
