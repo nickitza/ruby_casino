bbcf8738f8ced84250cd08a90ebe0152c8efcde8
tallan groberg <tallan.taven@gmail.com>
2019-05-10 22:45:00 -0600

    add player and dealer hands

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -5,31 +5,53 @@ require './aces.rb'
 deck = Deck.new
 
 class BlackJack
-
-
   def initialize
     game
   end
     def game
-
-
-
-      if @hand == 21
-      puts "you win"
-    else
+      puts "press 1 to play a hand"
+      input = gets.to_i
+      case input
+        @hand = DealCard
+      when @hand > 21
+      puts "bust"
+      when
        @hand < 21
       puts "1) would you like to hit again"
-    end
       input = gets.strip
+    else @hand == 21
+    rescue
+      puts "you win"
+    end
       case input
       when 1
         hit
     else
       puts "you lost"
-    end
+      end
   end
 end
 
+class DealCard
+
+  def initialize
+  random
+  end
+
+  def random
+  numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  numbers[rand(numbers.count)]
+
+  end
+
+  def random2
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  numbers[rand(numbers.count)]
+
+  end
+end
+
+ deal = DealCard.new.random + DealCard.new.random2
 
 
 BlackJack.new
--- blackjack/test.rb
+++ blackjack/test.rb
@@ -50,24 +50,51 @@
 # end
 # puts imperative_sum(numbers)
 
-# class DealCard
-#
-#   def initialize
-#   random
-#   end
-#
-#   def random
-#   numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
-#   numbers[rand(numbers.count)]
-#
-#   end
-#
-#   def random2
-#   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
-#   numbers[rand(numbers.count)]
-#
-#   end
-# end
-#
-#  deal = DealCard.new.random + DealCard.new.random2
-# puts deal
+class DealCard
+
+  def initialize
+  random
+  end
+
+  def random
+  numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  numbers[rand(numbers.count)]
+
+  end
+
+  def random2
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  numbers[rand(numbers.count)]
+  end
+
+end
+
+ @deal = DealCard.new
+
+
+class Round < DealCard
+  def initialize
+    playing
+  end
+  def playing
+    puts "press 1, lets play"
+    puts "press 2 to exit"
+    input = gets.to_i
+    case input
+    when 1
+      hand
+    when 2
+      exit
+
+    end
+  end
+  def hand
+    puts random + random2
+    dealer_hand
+  end
+  def dealer_hand
+    puts "#{random + random2} is dealers hand"
+  end
+end
+
+round = Round.new
