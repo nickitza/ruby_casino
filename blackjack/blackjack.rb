f3607d894ba58ef39cb646557205ce1d442ccaef
tallan groberg <tallan.taven@gmail.com>
2019-05-12 10:14:19 -0600

    basic hit method tweaks

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -15,23 +15,39 @@ class DealCard
   winner
   end
   def random
-  @numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  @numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
   @house = @numbers.sample(2)
   @dealercards = @house.reduce(:+)
   end
   def hit
-    puts "your total is :#{@playercards}, dealers card is :#{@house.first}, press one if you want to hit?"
+    puts "your total is :#{@playercards}, dealers card is :#{@house.first}."
+    puts "press 1 if you want to hit?"
+    puts "press 2 if you want to stay"
     take_hit = gets.to_i
     case take_hit
     when 1
       @the_hit = @numbers.sample(1)
       @the_hit << @playercards
+      # binding.pry
       @hit_total = @the_hit.reduce(:+)
+
       puts "you new total is #{@hit_total}"
+    when 2
+      winner
+    else
+      puts "you stayed with #{@playercards}"
     end
+    hit_till_quit
+  end
+    def hit_till_quit
+      if @hit_total > 21
+        hit
+      else
+        winner
     end
+  end
   def bets
-    new_num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+    new_num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     @gambler = new_num.sample(2)
     @playercards = @gambler.reduce(:+)
   end
@@ -60,12 +76,12 @@ class DealCard
   def winner
 
     case
+    when @dealercards > 21
+      puts "dealer busted"
     when @playercards < @dealercards
       puts "dealer wins"
     when @playercards > @dealercards
       puts "player wins"
-    when @dealercards > 21
-      puts "dealer busted"
     when @playercards > 21
       puts "player busted"
     when @playercards == @dealercards
